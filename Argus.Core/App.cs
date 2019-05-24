888235f96a131af16a660fa79dfffa999569188a
Jordon Malm <jordon.malm@gmail.com>
2019-05-23 20:07:33 -0500

    Add Core project and App class.

---
+++ Argus.Core/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Argus.Core
+{
+	/// <summary>
+	/// A simple representation of a company application
+	/// for the reporting dashboard.
+	/// </summary>
+	public class App
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int IssueCount { get; set; }
+		public string QueryString { get; set; }
+
+		public App()
+		{
+
+		}
+	}
+}
