6119830f7e83b0618bea8ab8f36bfd5685177aca
Jordon Malm <jordon.malm@gmail.com>
2019-08-31 20:26:36 -0500

    Add ChangeLog ViewComponent and static changelog HTML content.

---
+++ Argus.MVC/ViewComponents/ChangeLogViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Argus.MVC.ViewComponents
+{
+	public class ChangeLogViewComponent : ViewComponent
+	{
+		public ChangeLogViewComponent()
+		{
+
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			string changeLogContent = string.Empty;
+			string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/content/changelog.html");
+
+			using (var reader = File.OpenText(filePath))
+			{
+				changeLogContent = await reader.ReadToEndAsync();
+			}
+
+			return View("Default", changeLogContent);
+		}
+	}
+}
